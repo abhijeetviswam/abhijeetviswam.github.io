a3269ef98e82aa29535b52e10045fb43fb048e34
Abhijeet Viswam <abhijeetviswam@gmail.com>
2023-04-10 21:11:02 +0530

    [ui] Darkmode toggle switched moved to sidebar
    - Darkmode toggle switch is hidden if darktable is not reachable
    - localStorage color-scheme set to light on initial load

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -31,8 +31,22 @@ var myToggle = document.querySelector('.toggle');
 var myInitialContent = document.querySelector('.initial-content');
 var mySearchContent = document.querySelector('.search-content');
 var mySearchToggle = document.querySelector('.search-toggle');
+var myDarkModeIcon = document.querySelector('.darkmode-icon');
 var myDarkModeToggle = document.querySelector('.darkmode-toggle');
+var myDarkModeIconLight = document.querySelector('#toggle-light');
+var myDarkModeIconDark = document.querySelector('#toggle-dark');
 // var myDarkModeIcon = document.querySelector('.darkmode-icon');
+
+if (typeof DarkReader === 'undefined') {
+    // Don't display light dark toggle if darkreader is not reachable. i.e. no internet
+    myDarkModeToggle.style.visibility = "hidden";
+} else if (localStorage.getItem('color-scheme') === 'dark') {
+    myDarkModeIconDark.style.opacity = 1;
+    myDarkModeIconLight.style.opacity = 0.3;
+} else {
+    myDarkModeIconDark.style.opacity = 0.3;
+    myDarkModeIconLight.style.opacity = 1;
+}
 
 // Toggle sidebar visibility
 function toggleClassMenu() {
@@ -41,10 +55,12 @@ function toggleClassMenu() {
     myMenu.classList.add('is--visible');
     myToggle.classList.add('open');
     myWrapper.classList.add('is--pushed');
+    myDarkModeToggle.classList.add('darkmode-switch-is-visible');
   } else {
     myMenu.classList.remove('is--visible');
     myToggle.classList.remove('open');
     myWrapper.classList.remove('is--pushed');
+    myDarkModeToggle.classList.remove('darkmode-switch-is-visible');
   }
 }
 
@@ -110,6 +126,8 @@ function enableDarkMode() {
     DarkReader.setFetchMethod(window.fetch);
     DarkReader.enable({contrast: 120});
     localStorage.setItem('color-scheme', 'dark');
+    myDarkModeIconDark.style.opacity = 1;
+    myDarkModeIconLight.style.opacity = 0.3;
     // // Sun icon in dark mode
     // myDarkModeIcon.innerHTML = '<svg class="icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M7,12c0,2.8,2.2,5,5,5s5-2.2,5-5s-2.2-5-5-5S7,9.2,7,12z M12,9c1.7,0,3,1.3,3,3s-1.3,3-3,3s-3-1.3-3-3S10.3,9,12,9z"/><path d="M13,5V3c0-0.6-0.4-1-1-1s-1,0.4-1,1v2c0,0.6,0.4,1,1,1S13,5.6,13,5z"/><path d="M19.1,4.9c-0.4-0.4-1-0.4-1.4,0l-1.4,1.4c-0.4,0.4-0.4,1,0,1.4c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4   C19.5,6,19.5,5.3,19.1,4.9z"/><path d="M21,11h-2c-0.6,0-1,0.4-1,1s0.4,1,1,1h2c0.6,0,1-0.4,1-1S21.6,11,21,11z"/><path d="M17.7,16.2c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l1.4,1.4c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3c0.4-0.4,0.4-1,0-1.4   L17.7,16.2z"/><path d="M11,19v2c0,0.6,0.4,1,1,1s1-0.4,1-1v-2c0-0.6-0.4-1-1-1S11,18.4,11,19z"/><path d="M4.9,19.1c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3l1.4-1.4c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0l-1.4,1.4   C4.5,18,4.5,18.7,4.9,19.1z"/><path d="M2,12c0,0.6,0.4,1,1,1h2c0.6,0,1-0.4,1-1s-0.4-1-1-1H3C2.4,11,2,11.4,2,12z"/><path d="M6.3,4.9c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4l1.4,1.4C6.5,8,6.8,8.1,7.1,8.1S7.6,8,7.8,7.8c0.4-0.4,0.4-1,0-1.4L6.3,4.9z"/></svg>'
 }
@@ -117,6 +135,8 @@ function enableDarkMode() {
 function disableDarkMode() {
     DarkReader.disable();
     localStorage.setItem('color-scheme', 'light');
+    myDarkModeIconDark.style.opacity = 0.3;
+    myDarkModeIconLight.style.opacity = 1;
     // // Moon icon in light mode
     // myDarkModeIcon.innerHTML = '<svg class="icon" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="M12.3,4.9c0.4-0.2,0.6-0.7,0.5-1.1S12.2,3,11.7,3C6.8,3.1,3,7.1,3,12c0,5,4,9,9,9c3.8,0,7.1-2.4,8.4-5.9   c0.2-0.4,0-0.9-0.4-1.2c-0.4-0.3-0.9-0.2-1.2,0.1c-1,0.9-2.3,1.4-3.7,1.4c-3.1,0-5.7-2.5-5.7-5.7C9.4,7.8,10.5,5.9,12.3,4.9z    M15.1,17.4c0.5,0,1,0,1.4-0.1C15.3,18.4,13.7,19,12,19c-3.9,0-7-3.1-7-7c0-2.5,1.4-4.8,3.5-6c-0.7,1.1-1,2.4-1,3.8   C7.4,14,10.9,17.4,15.1,17.4z"/></svg>'
 }
